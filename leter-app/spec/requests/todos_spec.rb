49f6f7c6c784deab92e1ac28a810693bf359b56a
Jake Westerfield <jecobwesterfield@gmail.com>
2018-06-07 17:34:34 -0400

    we start by populating the database with a list of 10 record of files which we call todos for now. We use the factorybot gem to handle this population of data and we create within the custom helper method called json which parses our JSON response to a Ruby Hash

---
+++ leter-app/spec/requests/todos_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+RSpec.describe 'Todos API', type: :request do
+    #initialize test
+    let!(:todos) { create_list(:todo, 10) }
+    let(:todo_id) { todos.first.id }
+
+
+    # Test suite for GET /todos
+    describe 'GET/todos' do 
+        # make HTTP get request before each example
+        before { get '/todos' } 
+
+        it 'returns todos' do
+            #Note `json` is a custom helper top parse JSON responses
+            expect(json).not_to_be_empty
+            expect(json.size).to eq(10)
+        end
+
+        it 'returns status code 200' do
+            expect(response).to have_http_status(200)
+        end
+    end
+
+    # Test suite for GET /todos/:id
+    describe 'GET/todos/:id' do
+        before { get "/todos/#{todo_id}" }
+        
+        context 'when the record exists' do
+            it 'returns the todo' do
+                expect(json).not_to_be_empty
+
+                expect(json['id']).to.eq(todo_id)
+            end
+
+            it 'returns status code 200' do
+                expect(response).to have_http_status(200)
+            end
+        end
+
+        context 'when the record does not exist' do 
+            let(:todo_id) { 100 }
+
+            it 'returns status code 404' do
+                expect(response).to have_http_status(404)
+            end
+
+            it 'returns a not found message' do
+                expect(response.body).to match(/Couldn't find your Todo/)
+            end
+        end
+
+    end
+
+    # Test suite for POST /todos
+    describe 
