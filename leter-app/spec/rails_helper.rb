b1f2c30ee941cc57798f649db1105545ef243cb9
Jake Westerfield <jecobwesterfield@gmail.com>
2018-06-10 09:01:40 -0400

    fixed inproper formatting of item_spec.rb file, requires in wrong order

--- leter-app/spec/rails_helper.rb
+++ leter-app/spec/rails_helper.rb
@@ -1,18 +1,15 @@
-# require database cleaner 
-require 'database_cleaner'
-
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+
+require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-
+require 'database_cleaner'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -26,7 +23,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
@@ -72,7 +69,7 @@ RSpec.configure do |config|
   # add 'FactoryBot' methods
   config.include FactoryBot::Syntax::Methods
 
-  config.include RequestSpecHelper, type :request
+  config.include RequestSpecHelper, type: :request
 
   # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
   config.before(:suite) do
