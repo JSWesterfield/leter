74fdf2ec4c48b08e42f48a7f24650472e3f8d2c3
Jake Westerfield <jecobwesterfield@gmail.com>
2018-06-10 08:02:37 -0400

    Added in autoloading feature for our support directory, within the rails helper by including it as a shared module for all request specs in the RSpec configuration block.

--- leter-app/spec/rails_helper.rb
+++ leter-app/spec/rails_helper.rb
@@ -1,6 +1,9 @@
 # require database cleaner 
 require 'database_cleaner'
 
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -68,6 +71,8 @@ RSpec.configure do |config|
 
   # add 'FactoryBot' methods
   config.include FactoryBot::Syntax::Methods
+
+  config.include RequestSpecHelper, type :request
 
   # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
   config.before(:suite) do
