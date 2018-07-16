72ec88f538c6d8fe1347069a093ab67204ba69af
Jake Westerfield <jecobwesterfield@gmail.com>
2018-07-16 19:15:40 -0400

    when the todo doesnt exist return a response of 404 not found

--- leter-app/spec/requests/items_spec.rb
+++ leter-app/spec/requests/items_spec.rb
@@ -8,6 +8,29 @@ Rspec.describe 'Items API' do
     let(:id) { items.first.id }
 
 
+    # Test suite for GET /todos/:todo_id/items
+    describe 'GET /todos/:todo_id/items' do
+        before { get "/todos/#{todo_id}/items" }
 
+        context 'when todo exists' do
+            let(:todo_id) { 0 }    # Test suite for GET /todos/:todo_id/items
+            describe 'GET /todos/:todo_id/items' do
+                before { get "/todos/#{todo_id}/items" }
+        
+                context 'when todo exists' do
+                    let(:todo_id) { 0 }
 
+                    it 'returns status code 404' do
+                        expect(response).to match(/Couldn't find Todo/)
+                    end
+                end
+            end
+
+            
+            #Test suite for GET /todos/:todo_id/items/:id
+            describe 'GET /todos/:todo_id/items/:id' do
+                before { get "/todos/#{todo_id}/items/#{id}" }
+
+                context 'when todo item exists' do
+                    
             
